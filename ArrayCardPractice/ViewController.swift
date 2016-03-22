5014e01d93e1a4468620c0babdfd44c8a2508ea2
catranch <24inapril@gmail.com>
2016-03-22 15:36:12 -0700

    Initial Commit

---
+++ ArrayCardPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ArrayCardPractice
+//
+//  Created by Celena Haase on 3/22/16.
+//  Copyright © 2016 Celena Haase. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
