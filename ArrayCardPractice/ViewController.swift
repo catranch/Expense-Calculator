14513168c5b2c23de51e883bd02c0780c9c674d6
catranch <24inapril@gmail.com>
2016-03-23 16:58:09 -0700

    First run of Roommate Calculator

--- ArrayCardPractice/ViewController.swift
+++ ArrayCardPractice/ViewController.swift
@@ -8,18 +8,66 @@
 
 import UIKit
 
+struct Business {
+    let name: String
+}
+
+struct BizBill {
+    let restaurant: Business
+    var totalBill: Double
+    
+}
+
+let ralphs = Business(name:"Starter")
+let billR = BizBill(restaurant: ralphs, totalBill: 0.0)
+
+var bizBills = [billR]
+
+
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var taskInputFromUser: UITextField!
+    //put a variable here
+    @IBOutlet weak var taskAmountFromUser: UITextField!
+    
+    @IBOutlet weak var totalOwedRoomie: UILabel!
+    
+    @IBAction func submit(sender: UIButton) {
+        
+        let userInput = Business(name:taskInputFromUser.text!)
+       
+        let userAmountSpent = Double(taskAmountFromUser.text!)
+        
+        let billNext = BizBill(restaurant: userInput, totalBill:userAmountSpent!)
+       
+        bizBills.append(billNext)
+        
+        var grandTotal = 0.0 // sets to Double
+       
+        for bill in bizBills{
+            print(bill.restaurant.name)
+            print(bill.totalBill)
+       
+            grandTotal = grandTotal + bill.totalBill
+        }
+    
+        print("Grand Total")
+        print(grandTotal)
+        self.totalOwedRoomie.text = String(grandTotal)
+        self.taskInputFromUser.text = ""
+        self.taskAmountFromUser.text = ""
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        print("Greg Heo Strut Part6")
+        //var taskInputFromUser
+        
+        //put bizbills.append(bill3) down here? after
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
